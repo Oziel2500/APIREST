7b09ce10c4d0514fe1666c2ab7117f0e049c8c7e
Oziel <ozielp531@gmail.com>
2023-08-30 12:11:40 -0600

    Final

--- API REST/Parcial1/ModuloESM/funcion.js
+++ API REST/Parcial1/ModuloESM/funcion.js
@@ -1,7 +1,5 @@
-const frases = ["Hola", "Adios", "Bye", "Hello"];
-function generarNumeroAleatorio(min, max) { return Math.floor(Math.random() * (max - min + 1)) + min; }
-var numeroAleatorio = generarNumeroAleatorio(0, 3); // Genera un número entre 1 y 100
-export function ObtenerFrase() {
-    return frases[numeroAleatorio];
+let frases = ["Hola", "Adios", "Bye", "Hello"];
+export function ObtenerFrase(indice) {
+    return Promise.resolve(frases[indice]);
 }
 
