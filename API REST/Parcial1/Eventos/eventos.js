1d1b4da99c9941a1dbe07bab3382bdfdbc035b84
Oziel <ozielp531@gmail.com>
2023-09-22 07:34:23 -0600

    hi

--- API REST/Parcial1/Eventos/eventos.js
+++ API REST/Parcial1/Eventos/eventos.js
@@ -7,9 +7,9 @@ emisor.emit('Saludar', 'Daniel');
 
 function saludar() {
     const emisor = new events.EventEmitter();
-    setTimeout(() => emisor.emit('Saludar', 'nono'), 5000);
-    setTimeout(() => emisor.emit('Saludar', 'nana'), 500);
-    setTimeout(() => emisor.emit('Saludar', 'nora'), 2000);
+    setTimeout(() => emisor.emit('Saludar', 'si'), 5000);
+    setTimeout(() => emisor.emit('Saludar', 'no'), 500);
+    setTimeout(() => emisor.emit('Saludar', 'no lo se'), 2000);
     return emisor
 }
 
@@ -18,3 +18,5 @@ let sal = saludar();
 sal.on('Saludar', (nombre) => {
     console.log('hola ' + nombre)
 })
+
+//app.use(bearerToken(),validartoken());
